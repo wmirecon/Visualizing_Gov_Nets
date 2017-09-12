982869f1076f1e527816c3e069b44e16e7b0379a
wmirecon <srsche@gmail.com>
2017-09-12 17:47:00 -0400

    create v3-filtering for AA/DM/PT

---
+++ shiny/VGN-v3 12sep17/function helpers.R
@@ -0,0 +1,57 @@
+# this is a file for functions to be used in the shiny app
+
+
+
+# this function takes a vector of org names and returns a new edge list containing
+# the function relies on vectorized code to make sure it runs fast
+# the nodes that are to be included in the final edgelist are passed in a vector or org names
+# initial edgelist, respectively, area "to list" and a "from list" are generated where the 
+#   to node and the from node from the included in the passed vector of org names
+# edges must be in both lists to be included in the final edgelist
+# vectorization requires that only one column of data be considered at a time
+# to allow for comparison of one column, data are concatented into comparable string representations
+# only those edges with string representations found in both lists are included in the final data
+filter_el <- function( el, orgs ) {
+  
+  el.from <- el[el$from %in% orgs,]           # get edges where the from node is in the org vector ("from list")
+  el.to <- el[el$to %in% orgs,]               # get edges where the to node is in the org vector ("to list")
+  
+  
+  # get edge string represtations for the dyads in both the to and from lists
+  # the representations are appended as a new, 3rd column
+  el.from$string <- paste(el.from$from,
+                          "-",
+                          el.from$to,
+                          sep = "")
+  el.to$string <- paste(el.to$from,
+                        "-",
+                        el.to$to,
+                        sep = "")
+  
+  # select only those dyads where the string representation is in both lists
+  #  by getting the dyads in the from list that are also in the to list
+  el.filtered <- el.from[el.from$string %in% el.to$string,]
+  
+  return( unique( el.filtered[,-3]) )        # return the filtered edgelist, w/out the string representation
+}
+
+
+# this function is used to select the organizations that should be included based on AA/DM/PT selection
+# the app inputs will determine what index number(s) should be passed to the function
+# the index will indicate what AA/DM/PT has been selected
+# this function uses that selection to limit the org list to only those that use that AA/DM/PT
+select.orgs <- function( orgs, index.set ) {
+  col.set <- c( c(1:9), index.set )         # set of column indexes that contain attribute and filtering data
+  orgs.filter <- orgs[,col.set]             # select only the relevant AA/DM/PT data, plus initial attributes
+  if ( ncol( orgs.filter ) > 10 ) {         # PT is stored in three columns while AA/DM use only 1
+    # PT must have 1 in at least one of the three columns
+    # sum across columns to get a total
+    # will retain data where the sum is at least 1
+    # this will also hold for joining data from different tools, arenas, and domains
+    orgs.filter$sum <- rowSums(orgs.filter[,c(10:ncol(orgs.filter))])     # if PT, use row sums
+  } else {
+    colnames(orgs.filter)[10] <- "sum"      # if DM/AA, rename last column to match PT row sum column
+  }
+  orgs.select <- orgs.filter[orgs.filter$sum >= 1, ]     # filter based on sum column; AA/DM == 1; PT >= 1
+  return( orgs.select[,c(1:9)] )                         # return only the attr data from the filtered list
+}
---
+++ shiny/VGN-v3 12sep17/ui.R
@@ -0,0 +1,71 @@
+# ui.R for Visualizing Governance Net
+
+# load in the required R packages
+library( shiny )
+library( igraph )
+library( networkD3 )
+library( dplyr )
+library( RColorBrewer )
+
+# make sure strings are handled as strings
+options( stringsAsFactors = F )
+source("look up helpers.R")
+
+# setup in the user interface
+# options selected in the UI are stored in 'input'
+# specific inputs are labeled here and accessed in server.R as 'input$varname'
+shinyUI( fluidPage(
+  
+  # app title
+  titlePanel( "Visualizing Governance Networks-V2 25Aug17" ),
+  
+  # use a sidebar layout; controls on the left, map on the right
+  sidebarLayout(
+    sidebarPanel(
+      # setup subnet selection dropbox
+      selectInput( "net.select",                                     # input varname for functional subnet selection
+                   label = "Choose which subnetwork to display:",
+                   # list of values to include in the dropbox
+                   choice = subnet.choices
+                   ),
+      # setup domain input selection
+      selectInput( "dm.select",
+                   label = "Choose a policy domain:",
+                   choice = dm.choices
+                   ),
+      # setup policy tool input selection
+      selectInput( "pt.select",
+                   label = "Choose a policy tool focus:",
+                   choice = pt.choices
+                   ),
+      # setup action arenas input selection
+      selectInput( "aa.select",
+                   label = "Choose an action arena:",
+                   choice = aa.choices
+                   ),
+      # choose how to use the AA/DM/PT inputs - union or intersection
+      radioButtons( "join.method",
+                    label = "Choose a filtering method (not hooked up yet; currently uses union):",
+                    choices = c("Organizations in all of the selected filters (Intersection)" = 1,
+                                "Organizations in any of the selected filters (Union)"),
+                    selected = 1
+                    )
+      ), # end of sidebar panel
+    
+    # setup output panel
+    # values determined in server.R; accessed here by name assigned there, placed in quotes ("")
+    mainPanel(
+      h3("Please select a subnetwork"),
+      
+      # render the map made in server.R as plot output
+      forceNetworkOutput( "net.map", height = "650px" ),
+      
+      # setup text output for below the figure
+      p("The number after a node's name is its connection rate, reporting how many links the node has out of its possible links (normalized degree centrality)."),
+      br(),
+      h4("Please be patient with large networks. They are loading but may be slow."),
+      p("Use the selection criteria to view a smaller piece of the network.")
+      
+    ) # end of main panel
+  ) # end of sidebar layout
+))
