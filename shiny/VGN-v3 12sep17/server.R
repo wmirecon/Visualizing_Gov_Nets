365c9a0ec2673757239de73f7a7e6747e2a688fc
wmirecon <srsche@gmail.com>
2017-09-20 13:26:29 -0400

    add comments and some code refinement

--- shiny/VGN-v3 12sep17/server.R
+++ shiny/VGN-v3 12sep17/server.R
@@ -36,13 +36,22 @@ union.el <- unique( rbind( is.el, ta.el, pc.el, rt.el, fs.el ) )
 # generate a network map from the input options
 shinyServer( function ( input, output ) {
   
+  # Each time a setting is changed, this code will run and produce a map
+  # Steps:
+  #   1. Select the apprropriate subnetwork and load its links as the primary edgelist
+  #   2. Filter the organization list using the different filtering options
+  #   3. Filter the primary edgelist based on the org list filtering; only retain dyads
+  #      where both nodes are included in the filtered org list
+  #   4. Gather network data for use in map display
+  #   5. Convert data for mapping and make map.
+  
   # create the network map
   # map is rendered as visNetwork map and stored in the output list as 'net.map'
   
   output$net.map <- renderForceNetwork({
-    if( input$net.select != "Select a subnet" ) {
+    if( input$net.select != "Select a subnet" ) {        # if no subnetwork is chosen, skip all output to avoid an error message
       
-      # select an edge to use to make the network map
+      # select an edgelist to use to make the network map
       #print(input$net.select) #-- was used for bug testing
       el <- switch( input$net.select,
                     "Select a subnet" = data.frame(from = c(),
@@ -54,56 +63,87 @@ shinyServer( function ( input, output ) {
                     "Financial Resource Sharing" = fs.el,
                     "Union: All Subnets" =  union.el )
       
-      # filter node list - used now to speed up testing by getting a smaller network
-      # will later be used to select nodes for egonets and DM/AA/PT/AM selection
-      
+      # indicate which DM/PT/AA filters have been chosen and link to column indices in the org list
       pt.select <- as.numeric(pt.choice.index[input$pt.select])
       pt.select <- c(pt.select, pt.select + 12, pt.select + 24)
       aa.select <- as.numeric(aa.choice.index[input$aa.select])
       dm.select <- as.numeric(dm.choice.index[input$dm.select])
       
+      # filter nodes; if no filters are selected, step is by-passed
       if ( is.na(dm.select) && is.na(pt.select[1]) && is.na(aa.select ) ) {
         #nodes <- unique(head(org.list.bi.data[,c(1:9)], n = 50))
         nodes <- unique(org.list.bi.data[,c(1:9)])
       } else {
-        filter.select <- c(pt.select,
-                           aa.select,
-                           dm.select
-                           )
-        nodes.aa <- select.orgs( orgs = org.list.bi.data,
-                              index.set = na.omit( filter.select ) )
-        if ( input$join.method == 1 ) {
+        if ( input$join.method == 1 ) {                  # proceed here if using intersection of filters
           
+          # filtering for intersection works by joining together the filtered lists using each filter independently
+          # this org names from each separate filter are then joined into a common vector and the number of 
+          #   times that an organization appears in that vector is counted
+          # an organization must be observed in that vector at least as many times as there are filters applied
+          # this count is taken and the frequency count from the vector filtered to produce a filtered list or orgs
+          
+          # take the count of filters used
+          # if a filter is empty, it will be recorded as 'NA' (system missing)
+          # the filter is tested as being not NA and then the T/F transformed to 1/0
           filter.count <-
             as.numeric(!is.na(dm.choice.index[input$dm.select])) +
             as.numeric(!is.na(pt.choice.index[input$pt.select])) +
             as.numeric(!is.na(aa.choice.index[input$aa.select]))
           
+          # an output vector is created to store the combined results from individual filters
           raw.selection <- c()
-          if ( !is.na( dm.select ) ){
+          
+          # apply the individual filters
+          if ( !is.na( dm.select ) ) {       # policy domain
+            
+            # filter for all orgs involved in this domain and record their org.name
             additions <- select.orgs( orgs = org.list.bi.data,
                                       index.set = dm.select
                                       )$org.name
+            
+            # remove duplicates from this specific domain-specific filter
+            additions <- unique( additions )
+            
+            # add domain-based filter of organizations to the collective output
             raw.selection <- c( raw.selection, additions )
-          }
-          if ( !is.na( pt.select[1] ) ){
+          } # rinse and repeat for PT and AA
+          if ( !is.na( pt.select[1] ) ) {    # policy tool
             additions <- select.orgs( orgs = org.list.bi.data,
                                       index.set = pt.select
-            )$org.name
+                                      )$org.name
+            additions <- unique( additions )
             raw.selection <- c( raw.selection, additions )
           }
-          if ( !is.na( aa.select ) ){
+          if ( !is.na( aa.select ) ) {       # action arena
             additions <- select.orgs( orgs = org.list.bi.data,
                                       index.set = aa.select
-            )$org.name
+                                      )$org.name
+            additions <- unique( additions )
             raw.selection <- c( raw.selection, additions )
           }
           
+          # create and store a frequency table to get a count of how many times each org appears in the separate filters
           org.freq.df <- data.frame(table(raw.selection))
+          # it was more effective to save this as a data frame; that allowed for vectorized filtering
+          # efforts to use vectorized filtering without saving the dataframe failed, giving bad index value errors
+          
+          # filter frequency count dataframe based on filter.count
+          #   retain orgs that were in at least enough filters
           selected.orgs <- org.freq.df[org.freq.df$Freq >= filter.count,1]
+          
+          # filter overall orglist based on the above filtering results and retain attribute data
+          # this approach avoids using an additional left_join step and so should be faster
           nodes <- org.list.bi.data[org.list.bi.data$org.name %in% selected.orgs,c(1:9)]
-          nodes <- unique( nodes )
-        } else {
+          nodes <- unique( nodes )      # remove duplicate rows to avoid errors in making the network
+        } else {                                     # proceed here if using union of filters
+          
+          # join separate DM/PT/AA filtering info into a single vector
+          filter.select <- c(pt.select,
+                             aa.select,
+                             dm.select
+                             )
+          
+          # select all organizations that are in at least one of the filtering options
           nodes <- select.orgs( orgs = org.list.bi.data,
                                 index.set = na.omit( filter.select ) )
           nodes <- unique( nodes )
